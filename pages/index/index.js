97bd4baf4439729a338dd241d2e60ea8e22a26fd
yangliang <1902423082@qq.com>
2019-07-19 09:49:36 +0800

    新增nav
    新增nav

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    indicatorDots:true,
+    autoplay:true,
+    interval:3000,
+    duration:1000,
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    mov:{
+      inertia: true,
+      direction:"all",
+      outOfBounds:false,
+      x:1,
+      y:1,
+      damping:15,     // 阻尼系数
+      friction:2      // 摩擦系数
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  tap: function(e){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      mov:{
+        x: 300,
+        y: 300
+      }
+    });
   }
 })
